567d53d9171b0bd52b484a634308db675abef0fc
Berger Fotso <foberken@gmail.com>
2013-01-10 12:45:35 +0100

    [refactor] Auth test

--- test/services/auth-test.js
+++ test/services/auth-test.js
@@ -2,35 +2,38 @@ var vows = require('vows'),
     assert = require('assert'),
     xmlrpc = require('xmlrpc');
 
-var auth = require('../../lib/services/auth'),
-    createClient = require('../../lib/client').createClient,
+var createClient = require('../../lib/client').createClient,
     options = require('../config'),
-    client = createClient(options),
-    server = xmlrpc.createServer({ 'host': options.host, 'port': options.port }),
     fixtures = require('../fixtures/auth').login;
 
-
-server.on('login', function (err, params, callback) {
-  callback(null, fixtures.userId);
-});
-
-var client = createClient(options);
-
-vows.describe('auth').addBatch({
+vows.describe('services/auth').addBatch({
   'Authentification': {
-    'with correct credentials': {
+    'on an server set up': {
       topic: function () {
-        return client.auth;
-      },
-      'contain no error': function (auth) {
-        auth.login(function (err, userId) {
-          assert.isNull(err);
+        var server = xmlrpc.createServer({
+              'host': options.host,
+              'port': options.port
+            }, this.callback);
+
+        server.on('login', function (err, params, callback) {
+          callback(null, fixtures.userId);
         });
       },
-      'return a correct userid': function (auth) {
-        auth.login(function (err, userId) {
-          assert.deepEqual(userId, fixtures.userId);
-        });
+      'with correct credentials': {
+        topic: function () {
+          var client = createClient(options);
+          return client.auth;
+        },
+        'contain no error': function (auth) {
+          auth.login(function (err, userId) {
+            assert.isNull(err);
+          });
+        },
+        'return a correct userid': function (auth) {
+          auth.login(function (err, userId) {
+            assert.deepEqual(userId, fixtures.userId);
+          });
+        }
       }
     }
   }
